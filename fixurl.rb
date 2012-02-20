772f299c237572c9330b0116957ef72a124f5569
unknown <Art@Art-PC.(none)>
2012-02-20 11:58:49 +0700

    modified fixurl.rb

--- fixurl.rb
+++ fixurl.rb
@@ -5,6 +5,6 @@ system(cmd)
 ###
 cmd = "perl -p -i -e \"s/\/images/\/pmoph\/images/g\" app/views/layouts/extjs_layout.html.erb"
 system(cmd)
-##
+###
 cmd = "perl -p -i -e \"s/pre_url = \"\"/pre_url = \"\/pmoph\" public/javascripts/fshare.js"
 system(cmd)
